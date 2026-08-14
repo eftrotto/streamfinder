3bc33270969b15dfca383f553bb8499048ea2557
Enzo Trotto <enzofigueiratrotto@gmail.com>
2026-08-14 16:07:30 -0300

    Mocando retorno da rota de busca

--- backend/api.py
+++ backend/api.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Testando api"}
+
+@app.get("/busca/{user_busca}")
+async def read_item(user_busca):
+    return {"message": "O filme "+user_busca+" tem na netflix"}
